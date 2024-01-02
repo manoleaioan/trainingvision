0c0a77436837db72f354343f54bf037eadc41dbb
ioan <ioanmanolea@gmail.com>
2024-01-02 17:16:29 +0200

    up

--- script.js
+++ script.js
@@ -68,21 +68,18 @@ function generateRandomShapes() {
     }
 }
 
-function PrintElem(elem)
-{
+function PrintElem(elem) {
     var mywindow = window.open('', 'PRINT', 'height=3508,width=2480');
+    
+    mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+    mywindow.document.write('<link rel="stylesheet" href="style.css" onload="window.print();window.close();"></head><body>');
 
-    // mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-    // mywindow.document.write('</head><body >');
-    // mywindow.document.write('<h1>' + document.title  + '</h1>');
     mywindow.document.write(document.getElementById(elem).innerHTML);
+    
     mywindow.document.write('</body></html>');
-
+    
     mywindow.document.close(); // necessary for IE >= 10
-    mywindow.focus(); // necessary for IE >= 10*/
-
-    mywindow.print();
-    mywindow.close();
+    mywindow.focus(); // necessary for IE >= 10
 
     return true;
 }
