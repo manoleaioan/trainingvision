8b2b8f5f426e08e3aaf928bc719d899da4c4a4e4
ioan <ioanmanolea@gmail.com>
2024-07-31 20:10:01 +0300

    new option: change contrast

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const shapes = {
     circle: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100">
-        <circle cx="50" cy="50" r="44" stroke="black"/>
+        <circle cx="50" cy="50" r="44"/>
     </svg>`,
     square: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100">
-        <rect width="80" height="80" x="10" y="10" stroke="black"/>
+        <rect width="80" height="80" x="10" y="10"/>
     </svg>`,
     triangle: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100">
-        <polygon points="50,12 89,89 10,89" stroke="black"/>
+        <polygon points="50,12 89,89 10,89"/>
     </svg>`,
     hexagon: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100">
-        <polygon points="50,6 90,27.32 90,72.68 50,94 10,72.68 10,27.32" stroke="black"/>
+        <polygon points="50,6 90,27.32 90,72.68 50,94 10,72.68 10,27.32"/>
     </svg>`,
     plus: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 100">
-        <rect width="1" height="80" x="49.5" y="10" stroke="black"/>
-        <rect width="80" height="1" x="10" y="49.5" stroke="black"/>
+        <rect width="1" height="80" x="49.5" y="10"/>
+        <rect width="80" height="1" x="10" y="49.5"/>
     </svg>`,
-    // foursquare: `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" stroke="black" viewBox="0 0 200 200" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs"><path  d="M165.963 134.037c-5.467 5.467-14.332 5.467-19.799 0l-24.137-24.138c-5.468-5.467-5.468-14.331 0-19.799l24.137-24.137c5.467-5.467 14.332-5.467 19.799 0L190.101 90.1c5.467 5.468 5.467 14.332 0 19.799l-24.138 24.138Zm-112.127 0c-5.467 5.467-14.332 5.467-19.8 0L9.9 109.899c-5.468-5.467-5.468-14.331 0-19.799l24.137-24.137c5.467-5.467 14.332-5.467 19.799 0L77.973 90.1c5.468 5.468 5.468 14.332 0 19.799l-24.137 24.138ZM109.9 190.1c-5.468 5.468-14.332 5.468-19.8 0l-24.137-24.137c-5.467-5.467-5.467-14.332 0-19.799l24.138-24.137c5.467-5.468 14.331-5.468 19.799 0l24.137 24.137c5.467 5.467 5.467 14.332 0 19.799L109.9 190.1Zm0-112.127c-5.468 5.468-14.332 5.468-19.8 0L65.963 53.836c-5.467-5.468-5.467-14.332 0-19.8L90.101 9.9c5.467-5.467 14.331-5.467 19.799 0l24.137 24.138c5.467 5.467 5.467 14.331 0 19.799L109.9 77.973Z"></path></svg>`,
+    // foursquare: `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs"><path  d="M165.963 134.037c-5.467 5.467-14.332 5.467-19.799 0l-24.137-24.138c-5.468-5.467-5.468-14.331 0-19.799l24.137-24.137c5.467-5.467 14.332-5.467 19.799 0L190.101 90.1c5.467 5.468 5.467 14.332 0 19.799l-24.138 24.138Zm-112.127 0c-5.467 5.467-14.332 5.467-19.8 0L9.9 109.899c-5.468-5.467-5.468-14.331 0-19.799l24.137-24.137c5.467-5.467 14.332-5.467 19.799 0L77.973 90.1c5.468 5.468 5.468 14.332 0 19.799l-24.137 24.138ZM109.9 190.1c-5.468 5.468-14.332 5.468-19.8 0l-24.137-24.137c-5.467-5.467-5.467-14.332 0-19.799l24.138-24.137c5.467-5.468 14.331-5.468 19.799 0l24.137 24.137c5.467 5.467 5.467 14.332 0 19.799L109.9 190.1Zm0-112.127c-5.468 5.468-14.332 5.468-19.8 0L65.963 53.836c-5.467-5.468-5.467-14.332 0-19.8L90.101 9.9c5.467-5.467 14.331-5.467 19.799 0l24.137 24.138c5.467 5.467 5.467 14.331 0 19.799L109.9 77.973Z"></path></svg>`,
     // cube: `<svg xmlns="http://www.w3.org/2000/svg" id="SvgjsSvg1130" width="200" height="200" viewBox="0 0 512 512" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs"><path d="m256.002 242.913 210.412-121.43L256.002 0 45.586 121.483zm-15.053 26.073L30.534 147.557v242.96L240.949 512zm30.107 0V512l210.41-121.483v-242.96z"></path></svg>`,
     // drop: `
     // <svg viewBox="38.656 272.6229 140 195.6021" width="140" height="195.6021" xmlns="http://www.w3.org/2000/svg">
     //   <path d="M 178.656 397.721 C 178.656 436.659 147.316 468.225 108.656 468.225 C 69.996 468.225 38.656 436.659 38.656 397.721 C 38.656 360.864 74.496 316.431 103.769 275.179 C 106.169 271.798 111.194 271.764 113.63 275.119 C 144.007 316.962 178.656 360.816 178.656 397.721 Z" style="fill: rgba(0, 0, 0, 0); stroke: rgb(0, 0, 0);" transform="matrix(0.9999999999999999, 0, 0, 0.9999999999999999, 0, 0)"/>
     // </svg>`,
     heart: `<svg width="100%" height="100%" viewBox="78 20 200 250" xmlns="http://www.w3.org/2000/svg">
-      <path d="M 270.762 66.279 C 259.428 53.986 243.877 47.216 226.968 47.216 C 214.33 47.216 202.755 51.212 192.565 59.091 C 187.424 63.068 182.765 67.935 178.657 73.614 C 174.551 67.936 169.891 63.068 164.748 59.091 C 154.559 51.212 142.985 47.216 130.346 47.216 C 113.438 47.216 97.885 53.986 86.551 66.279 C 75.352 78.428 69.183 95.026 69.183 113.016 C 69.183 131.533 76.084 148.484 90.899 166.36 C 104.152 182.352 123.2 198.585 145.258 217.383 C 152.791 223.802 161.328 231.079 170.193 238.829 C 172.535 240.881 175.544 242.011 178.657 242.01 C 181.769 242.011 184.776 240.882 187.118 238.833 C 195.983 231.08 204.526 223.801 212.061 217.378 C 234.116 198.583 253.164 182.352 266.418 166.359 C 281.233 148.484 288.132 131.533 288.132 113.015 C 288.132 95.026 281.963 78.428 270.762 66.279 Z M 270.762 66.279" style="stroke-width: 24px; stroke: rgb(0, 0, 0);"></path>
+      <path d="M 270.762 66.279 C 259.428 53.986 243.877 47.216 226.968 47.216 C 214.33 47.216 202.755 51.212 192.565 59.091 C 187.424 63.068 182.765 67.935 178.657 73.614 C 174.551 67.936 169.891 63.068 164.748 59.091 C 154.559 51.212 142.985 47.216 130.346 47.216 C 113.438 47.216 97.885 53.986 86.551 66.279 C 75.352 78.428 69.183 95.026 69.183 113.016 C 69.183 131.533 76.084 148.484 90.899 166.36 C 104.152 182.352 123.2 198.585 145.258 217.383 C 152.791 223.802 161.328 231.079 170.193 238.829 C 172.535 240.881 175.544 242.011 178.657 242.01 C 181.769 242.011 184.776 240.882 187.118 238.833 C 195.983 231.08 204.526 223.801 212.061 217.378 C 234.116 198.583 253.164 182.352 266.418 166.359 C 281.233 148.484 288.132 131.533 288.132 113.015 C 288.132 95.026 281.963 78.428 270.762 66.279 Z M 270.762 66.279" style="stroke-width: 24px;"></path>
     </svg>`,
 };
 
@@ -42,6 +42,8 @@ var generatedShapes = "";
 var ChangeShapeSizeTimeoutId = 0;
 var holdInterval;
 var saveTimeout;
+
+
 
 function getRandomSize() {
     return Math.floor(Math.random() * 100) + 50;
@@ -83,7 +85,7 @@ function generateRandomShapes(generateAlphaNumeric = false) {
 
         shapeDiv.style.width = shapeSize + "px";
         shapeDiv.style.height = shapeSize + "px";
-        shapeDiv.style.fontSize = shapeSize + 30 + "px";
+        shapeDiv.style.fontSize = shapeSize + 30+ "px";
 
         shapeDiv.style.strokeWidth = strokeWidth + "px";
         shapeDiv.style.stroke = color;
@@ -162,9 +164,9 @@ function PrintElem() {
     return true;
 }
 
-function ChangeContrast() {
+function ChangeContrast(){
     contrast = !contrast;
-    color = contrast ? "white" : "black";
+    color =  contrast ? "white" : "black";
 
     Ui_updateContrast();
 
@@ -202,7 +204,7 @@ function ChangeShapeSize(increase) {
     if (shapeSize < 20) {
         shapeSize = 20;
         return;
-    }
+    } 
 
     updateShapes(shapes => {
         shapes.querySelectorAll('.item').forEach(shape => {
@@ -243,7 +245,16 @@ function Ui_updateContrast() {
         document.getElementById('light').style.display = 'none';
     }
 
-    document.documentElement.classList = contrast ? 'inverted' : '';
+    document.documentElement.style.background =  !contrast ? "white" : "black";
+
+    updateShapes(shapes => {
+        shapes.querySelectorAll('.item').forEach(shape => {
+            shape.style.color = color;
+            shape.style.stroke = color;
+            shape.style.fill = !outline ? color : "transparent";
+
+        });
+    });
 }
 
 function Ui_updatePageFormat() {
@@ -269,7 +280,7 @@ function Ui_updateShapeFillType() {
 
     updateShapes(shapes => {
         shapes.querySelectorAll('.item').forEach(shape => {
-            shape.style.fill = !outline ? "black" : "transparent";
+            shape.style.fill = !outline ? color : "transparent";
         });
     });
 }
@@ -307,6 +318,7 @@ function loadLocal() {
         outline = storedJsonString.outline;
         contrast = storedJsonString.contrast;
         alphaNum = storedJsonString.alphaNum;
+        color = !contrast ? 'black': 'white';
     }
 }
 
